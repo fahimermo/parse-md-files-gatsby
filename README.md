61a5628bad9eee5f0107df754bac32f4ffeac40b
elegant-moon <elegantmoon.tech@gmail.com>
2020-04-17 16:56:50 +0200

    Add screencapture image

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MD Parser for gatsby
+
+![screenshot](./screencapture-localhost-8000-2020-04-17-16_55_30.png)
--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@
 
 module.exports = {
   /* Your site config here */
+  pathPrefix: "/parse-md-files-gatsby",
   siteMetadata: {
     title: `MD Parser`,
     description: `MD Parser for Gatsby`,
